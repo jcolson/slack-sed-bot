630d7fea71497e77749932dacfad40e67089c8ae
Jay Colson <jay@karma.net>
2019-03-10 23:30:56 +0000

    got the imagemagick working

--- sedbot.js
+++ sedbot.js
@@ -3,6 +3,8 @@
 const os = require('os');
 var https = require('https');
 var Ws = require('ws');
+var im = require('imagemagick');
+var tmp = require('tmp');
 
 class Sedbot {
   constructor(config) {
@@ -28,8 +30,113 @@ class Sedbot {
     // console.log(JSON.stringify(user));
     });
   }
+  onCommandHelp() {
+    let commandText = 'Just use something simple like:\n';
+    commandText += '`s/text to replace/text replaced with`\n';
+    commandText += 'or\n';
+    commandText += '`s/[tT]ext to replace/text replaced with/g`\n';
+    commandText += 'or try a command:\n';
+    commandText += '`.help`                - this help\n';
+    commandText += '`.wtr [location]?[m/u]`- get the current weather for [location]. [m] == metric, [u] == USCS\n';
+    commandText += '`.about`               - get information about bot\n';
+    commandText += '`.ping`                - ping the bot\n';
+    return commandText;
+  }
+  onCommandPing() {
+    let commandText = 'PONG\n';
+    return commandText;
+  }
+  onCommandAbout() {
+    let commandText = 'OS: ' + os.platform() + ' / ' + os.release() + '\n';
+    commandText += 'Host: ' + os.hostname() + '\n';
+    commandText += 'Uptime: ' + (os.uptime() / 60 / 60 / 24).toFixed(2) + ' days\n';
+    commandText += 'Load: ' + os.loadavg()[0].toFixed(2) + ' / ' +  os.loadavg()[1].toFixed(2) + ' / ' + os.loadavg()[2].toFixed(2) + '\n';
+    return commandText;
+  }
+  onCommandUSA(parameters) {
+    var tmpobj = tmp.fileSync({ mode: '0644', prefix: 'sedbot-', postfix: '.png' });
+    console.log('File: ', tmpobj.name);
+    console.log('Filedescriptor: ', tmpobj.fd);
+    let imCommandLine = ['-background', 'transparent'];
+    for (let i = 0; i < parameters.length; i++) {
+      let currentChar = parameters.substring(i, i + 1);
+      imCommandLine.push('\(');
+      imCommandLine.push('-fill');
+      if (i % 3 === 0) {
+        imCommandLine.push('red');
+      } else if (i % 3 === 1) {
+        imCommandLine.push('white');
+      } else if (i % 3 === 2) {
+        imCommandLine.push('blue');
+      }
+      imCommandLine.push('-font');
+      imCommandLine.push('Helvetica');
+      imCommandLine.push('-pointsize');
+      imCommandLine.push('16');
+      imCommandLine.push('label:' + currentChar);
+      imCommandLine.push('\)');
+    }
+    imCommandLine.push('+append');
+    imCommandLine.push(tmpobj.name);
+    im.convert(imCommandLine,
+      function(err, stdout){
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('stdout:', stdout);
+        }
+      });
+    let commandText = 'convert';
+    for (let commandParam of imCommandLine) {
+      commandText += ' ';
+      commandText += commandParam;
+    }
+    return commandText;
+  }
+  onCommandWeather(parameters, wsc, messageData) {
+    let format = 'format=4';
+    if (parameters.indexOf('?') === -1) {
+      format = '?' + format;
+    } else {
+      format = '&' + format;
+    }
+    let options = {
+      protocol: 'https:',
+      host: 'wttr.in',
+      port: '443',
+      path: encodeURI('/' + parameters + format),
+      headers: {
+        Accept: 'text/plain',
+        'User-Agent': 'like curl',
+      },
+    };
+    https.get(options, (response) => {
+      // console.log('statusCode:', response.statusCode);
+      // console.log('headers:', response.headers);
+      let body = '';
+      response.on('data', (data) => {
+        body += data;
+      });
+      response.on('end', function() {
+        // console.log(body);
+        if (body.indexOf('Error') !== -1) {
+          body = 'Error encountered, try a different location';
+        }
+        let sendCommandData = {
+          type: 'message',
+          channel: messageData.channel,
+          text: body,
+        };
+        wsc.send(JSON.stringify(sendCommandData));
+      });
+    }).on('error', (e) => {
+      console.error('received error: ' + e.message);
+    });
+    return '';
+  }
   handleCommands(messageData, wsc) {
-    let commands = ['HELP', 'PING', 'ABOUT', 'WTR'];
+    const self = this;
+    let commands = ['HELP', 'PING', 'ABOUT', 'WTR', 'USA'];
     let sedId = '<@' + this.userMapByName['sed'].id + '> ';
     let commandMatch = null;
     let parameters = null;
@@ -41,83 +148,29 @@ class Sedbot {
     } else if (messageData.text.startsWith('.')) {
       substringFrom = '.'.length;
     }
-    // console.log('substringFrom: '+substringFrom);
     let substringTo = messageData.text.substring(substringFrom).indexOf(' ');
     if (substringTo === -1) {
       substringTo = messageData.text.length;
     } else {
       substringTo = substringTo + substringFrom;
     }
-    // console.log('substringTo: ' + substringTo);
     let possibleCommand = messageData.text.substring(substringFrom, substringTo).toUpperCase();
-    // console.log('possibleCommand: ' + possibleCommand);
     if (commands.includes(possibleCommand)) {
       commandMatch = possibleCommand;
       parameters = messageData.text.substring(substringTo + 1);
     }
-    // console.log('commandmatch: '+commandMatch);
-    // console.log('parameters: '+parameters);
-
-    // console.log('commandMatch = "' + commandMatch + '"');
     if (commandMatch !== null) {
       let commandText = '';
-      // console.log('matched a command: ' + commandMatch);
       if (commandMatch === 'HELP') {
-        commandText = 'Just use something simple like:\n';
-        commandText += '`s/text to replace/text replaced with`\n';
-        commandText += 'or\n';
-        commandText += '`s/[tT]ext to replace/text replaced with/g`\n';
-        commandText += 'or try a command:\n';
-        commandText += '`.help`                - this help\n';
-        commandText += '`.wtr [location]?[m/u]`- get the current weather for [location]. [m] == metric, [u] == USCS\n';
-        commandText += '`.about`               - get information about bot\n';
-        commandText += '`.ping`                - ping the bot\n';
+        commandText = self.onCommandHelp();
       } else if (commandMatch === 'PING') {
-        commandText = 'PONG\n';
+        commandText = self.onCommandPing();
       } else if (commandMatch === 'ABOUT') {
-        commandText = 'OS: ' + os.platform() + ' / ' + os.release() + '\n';
-        commandText += 'Host: ' + os.hostname() + '\n';
-        commandText += 'Uptime: ' + (os.uptime() / 60 / 60 / 24).toFixed(2) + ' days\n';
-        commandText += 'Load: ' + os.loadavg()[0].toFixed(2) + ' / ' +  os.loadavg()[1].toFixed(2) + ' / ' + os.loadavg()[2].toFixed(2) + '\n';
+        commandText = self.onCommandAbout();
       } else if (commandMatch === 'WTR') {
-        let format = 'format=4';
-        if (parameters.indexOf('?') === -1) {
-          format = '?' + format;
-        } else {
-          format = '&' + format;
-        }
-        let options = {
-          protocol: 'https:',
-          host: 'wttr.in',
-          port: '443',
-          path: encodeURI('/' + parameters + format),
-          headers: {
-            Accept: 'text/plain',
-            'User-Agent': 'like curl',
-          },
-        };
-        https.get(options, (response) => {
-          // console.log('statusCode:', response.statusCode);
-          // console.log('headers:', response.headers);
-          let body = '';
-          response.on('data', (data) => {
-            body += data;
-          });
-          response.on('end', function() {
-            // console.log(body);
-            if (body.indexOf('Error') !== -1) {
-              body = 'Error encountered, try a different location';
-            }
-            let sendCommandData = {
-              type: 'message',
-              channel: messageData.channel,
-              text: body,
-            };
-            wsc.send(JSON.stringify(sendCommandData));
-          });
-        }).on('error', (e) => {
-          console.error('received error: ' + e.message);
-        });
+        commandText = self.onCommandWeather(parameters, wsc, messageData);
+      } else if (commandMatch === 'USA') {
+        commandText = self.onCommandUSA(parameters);
       }
       if (commandText !== '') {
         let sendCommandData = {
@@ -157,12 +210,12 @@ class Sedbot {
         for (var i = this.history[messageData.channel].length - 1; i >= 0; i--) {
           if (matcher.test(this.history[messageData.channel][i].text)) {
             // Matching message found, send the replacement and exit.
-
             // Fallback user, in case someone new joined. Not handled ATM.
             var sender = '_Unknown user_';
             try {
               sender = this.userMap[this.history[messageData.channel][i].user].real_name;
             } catch (e) {
+              console.error('Didn\'t find user in map');
             }
             var newText = 'Correction, *' + sender + '* ...\n' + this.history[messageData.channel][i].text.replace(matcher, ' *' + sedMatch[3] + '* ');
             var sendData = {
